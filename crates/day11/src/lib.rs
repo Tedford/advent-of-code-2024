175b4baf8a39a79cbbc3b609fb483f72c33ddf92
Tedford <dev@tedfordjohnson.com>
2025-02-08 12:07:14 -0500

    day 11 part 2

--- crates/day11/src/lib.rs
+++ crates/day11/src/lib.rs
@@ -1,11 +1,9 @@
-#[derive(Eq, PartialEq)]
+use std::collections::HashMap;
+
+#[derive(Eq, PartialEq, Debug)]
 struct Stone {
     value: i64,
     count: i64,
-}
-
-fn parse(input: &Vec<String>) -> Vec<i64> {
-    input.iter().map(|x| x.split_whitespace()).flatten().map(|x| x.parse::<i64>().unwrap()).collect()
 }
 
 fn parse_stones(input: &Vec<String>) -> Vec<Stone> {
@@ -35,23 +33,13 @@ pub fn blink(stones: &Vec<i64>) -> Vec<i64> {
     result
 }
 
-fn blinks(stones: Vec<i64>, cycles: i32) -> Vec<i64> {
-    let mut result = stones;
-
-    print!("Starting:");
-    for i in 0..cycles {
-        if i % 5 == 0 {
-            print!("..{cycles}")
-        }
-        result = blink(&result);
-    }
-    println!("Done");
-    result
-}
-
 pub fn part1(input: &Vec<String>) -> i64 {
-    let stones = blinks(parse(input), 25);
-    stones.len() as i64
+    let mut stones = parse_stones(&input);
+    for _ in 0..25 {
+        stones = blink_and_condense(&stones);
+    }
+
+    stones.iter().map(|s| s.count).sum()
 }
 
 pub fn part2(input: &Vec<String>) -> i64 {
@@ -64,30 +52,30 @@ pub fn part2(input: &Vec<String>) -> i64 {
 }
 
 fn blink_and_condense(stones: &Vec<Stone>) -> Vec<Stone> {
-    let mut result = vec![];
+    let mut cache: HashMap<i64, i64> = HashMap::new();
 
     for stone in stones {
         match stone.value {
-            0 => result.push(Stone { value: 1, count: 1 }),
+            0 => *cache.entry(1).or_insert(0) += stone.count,
             x if x.to_string().len() % 2 == 0 => {
                 let text = x.to_string();
                 let left = text[0..text.len() / 2].parse::<i64>().unwrap();
                 let right = text[text.len() / 2..text.len()].parse::<i64>().unwrap();
-                result.push(Stone { value: left, count: 1 });
-                result.push(Stone { value: right, count: 1 });
+                *cache.entry(left).or_insert(0) += stone.count;
+                *cache.entry(right).or_insert(0) += stone.count;
+            },
+            x => {
+                let key = x * 2024;
+                *cache.entry(key).or_insert(0) += stone.count;
             }
-            x => result.push(Stone { value: x * 2048, count: 1 }),
         }
     }
-
-
-    result
+    cache.into_iter().map(|(value, count)| Stone { value, count }).collect()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use rstest::rstest;
 
     fn get_sample() -> Vec<String> {
         include_str!("sample.dat")
@@ -103,26 +91,6 @@ mod tests {
         assert_eq!(result, vec![1, 2024, 1, 0, 9, 9, 2021976]);
     }
 
-    #[rstest]
-    #[case::one_blink(1, vec![125, 17], vec![253000, 1, 7])]
-    #[case::two_blinks(2, vec![125, 17], vec![253, 0, 2024, 14168])]
-    #[case::three_blinks(3, vec![125, 17], vec![512072, 1, 20, 24, 28676032])]
-    #[case::four_blinks(4, vec![125, 17], vec![512, 72, 2024, 2, 0, 2, 4, 2867, 6032])]
-    #[case::five_blinks(5, vec![125, 17], vec![1036288, 7, 2, 20, 24, 4048, 1, 4048, 8096, 28, 67, 60, 32]
-    )]
-    #[case::six_blinks(6, vec![125, 17], vec![2097446912, 14168, 4048, 2, 0, 2, 4, 40, 48, 2024, 40, 48, 80, 96, 2, 8, 6, 7, 6, 0, 3, 2]
-    )]
-    fn blinks_valid(#[case]cycles: i32, #[case] start: Vec<i64>, #[case] target: Vec<i64>) {
-        assert_eq!(blinks(start, cycles), target);
-    }
-
-    #[test]
-    fn blink_length() {
-        let result = blinks(vec![125i64, 17i64], 6);
-        println!("{:?}", result);
-        assert_eq!(result.len(), 22);
-    }
-
     #[test]
     fn sample_part1_full() {
         let sample = get_sample();
@@ -134,6 +102,6 @@ mod tests {
     fn sample_part2_valid() {
         let sample = get_sample();
         let result = part2(&sample);
-        assert_eq!(result, 0);
+        assert_eq!(result, 65601038650482);
     }
 }
